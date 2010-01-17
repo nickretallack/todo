db2f55abe3928926249232348669fd59c1008371
Nick Retallack <nickretallack@gmail.com>
2010-01-17 01:10:57 -0800

    smart search works nicely.  Trying to re-write this auto-complete stuff to do what I want

--- util.js
+++ util.js
@@ -1,3 +1,88 @@
+// function dict_keys(dict){
+//     var keys = []
+//     for (var key in dict){
+//         if (dict.hasOwnProperty(key)){
+//             keys.push(key)
+//         }
+//     }
+//     return items    
+// }
+
+
+function dict_items(dict){
+    var items = []
+    for (var key in dict){
+        if (dict.hasOwnProperty(key)){
+            items.push({key:key, value:dict[key]})
+        }
+    }
+    return items
+}
+
+function compare_by_value(left, right){
+    return right.value - left.value
+}
+
+// Search for every non-stop-word.  Only include stemming on the last word.
+// Score results by how many searches they came up in
+function smarter_search(string){
+    var words = cull_stopwords(to_words(string))
+    if (words.length == 0) return [] // Don't bother searching if there's no input
+
+    // Stem search on the last word, since it's probably the one you're currently typing
+    // But don't bother until you've input a few letters.  Otherwise the result set is too big.
+    if (words[words.length-1].length > 2) {
+        words[words.length-1] += "*"        
+    }
+
+    // NOTE: change this to match the whole item once I remove the stemming column
+    var results = {}
+    $(words).each(function(){
+        var word = this
+        console.debug("searching for ", JSON.stringify(this))
+        query_iterate( db, 'select text from item where text match ?', [word], function(row){
+            var text = row.text
+            if (typeof results[text] == 'undefined') results[text] = 0
+            results[text] += 1
+        }, "unsafe_mode")
+    })
+    
+    // TODO: consider normalizing the value to use as 
+    var sorted = dict_items(results).sort(compare_by_value)
+    var values = []
+    // TODO: yeah I really need a good 'map'.  Consider underscore?
+    $(sorted).each(function(){
+        values.push(this.key)
+    })
+    console.debug(results, sorted)
+    
+    return values
+}
+
+function test_smarter_search(){
+    var db = google.gears.factory.create('beta.database');
+    db.open('test_smarter_search');
+    db.execute('create virtual table item using fts2(text, note)')
+    var inputs = ['first second third', 'first second', 'first second fourth', 'first third fourth']
+    $(inputs).each(function(){
+        db.execute('insert into item (text) values (?)', [this])
+    })
+    
+    smarter_search
+
+
+    db.remove()
+}
+
+
+
+function cull_stopwords(list){
+    // TODO: implement this.  Get the stopwords list from google.
+    return list
+}
+
+
+// TODO: add to result set prototype
 function as_dict(result_set){
     results = {}
     for (var index = result_set.fieldCount()-1; index >= 0; index--){
@@ -6,8 +91,15 @@ function as_dict(result_set){
     return results
 }
 
-function query_iterate(query, params, callback){
-    var result_set = db.execute(query, params)
+function query_iterate(database, query, params, callback, unsafe_mode){
+    if (unsafe_mode){
+        // Gears database layer is acting really crummy and not substituting in
+        // my parameters properly, so I'll substitute them myself.  Grr.  TODO: file bug somehow
+        query = query.replace(/\?/, JSON.stringify(params.pop()))
+    }
+    
+    
+    var result_set = database.execute(query, params)
     for (; result_set.isValidRow(); result_set.next()) {
         var row = as_dict(result_set)
         callback(row)
@@ -15,8 +107,13 @@ function query_iterate(query, params, callback){
     result_set.close()
 }
 
+function to_words(string){
+    return string.trim().split(new RegExp("\\s+"))
+}
+
+
 function generate_stems(text){
-    var words = text.trim().split(new RegExp("\\s+"));
+    var words = to_words(text)
     var stems = {}
     $(words).each(function(){
         var word = this
