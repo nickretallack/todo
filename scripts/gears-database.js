4d2127d70d96b35dabdd60af673cda15e303f64f
Nick Retallack <nickretallack@gmail.com>
2010-01-31 19:09:14 -0800

    added an (untested) exporter

--- scripts/gears-database.js
+++ scripts/gears-database.js
@@ -190,3 +190,43 @@ function get_finished_items(){
     from item join item_text on item.rowid = item_text.rowid \
     where item.done_date is not null')
 }
+
+function export_data(){
+    // Don't use any helper methods in this library.  This needs to be the raw data, un-prettified
+    var items = db.selectAll('select item.*, item_text.text, item_text.note \
+    from item join item_text on item.rowid = item_text.rowid')
+    var prerequisites = db.selectAll('select * from prerequisite')
+    
+    var data = {items:items, prerequisites:prerequisites}
+    return JSON.stringify(data)
+}
+
+function filter_fields(obj, keys, want){
+    results = {}
+    _.each(obj, function(value, key, list) {
+        var includes = _.include(keys, key)
+        if ((want && includes) || (!want && !includes)) results[key] = value
+    });
+    return results
+}
+
+
+function import_data(string_data){
+    var data = JSON.parse(string_data)
+    var item_text_keys = ['text', 'note']
+    db.transaction(function(db){
+        
+        // insert items
+        _.each(data.items, function(item){
+            var item_details = filter_fields(item, item_text_keys, false)
+            db.insert('item', item_details)
+            db.do('insert into item_text (rowid, text, note) values (last_insert_rowid(), ?, ?)', [item.text, item.note]);
+        })
+
+        // insert prerequisites
+        _.each(data.prerequisites, function(prerequisite){
+            db.insert('prerequisite', prerequisite)
+        })
+    })
+}
+
